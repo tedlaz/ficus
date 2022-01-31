bedce205ec5323b09553ec65d18198903a3fde68
Ted Lazaros <tedlaz@gmail.com>
2022-02-01 00:58:19 +0200

    appv3

--- externals.ps1
+++ externals.ps1
@@ -1,11 +1,7 @@
-cd external
+mkdir ex1
+cd ex1
 curl -L -o ffmpg.7z https://www.gyan.dev/ffmpeg/builds/ffmpeg-git-essentials.7z
-7z x ffmpg.7z
-del ffmpg.7z
-ren ffmpeg* ffmpeg
-copy ffmpeg/bin/ffmpeg.exe .
-copy ffmpeg/bin/ffprobe.exe .
+7z e ffmpg.7z  *.exe -r
 copy C:\Windows\SysWOW64\msvcr100.dll .
-7z x msvcr100.zip
 curl -L -o youtube-dl.exe https://youtube-dl.org/downloads/latest/youtube-dl.exe
 cd ..
